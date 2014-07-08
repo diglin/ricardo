4ea3e212c2c593644844d3b87a197395284e7122
Sylvain Rayé <sly@diglin.com>
2014-07-09 00:08:46 +0200

    [RIC-28] [FEATURE] Set new category attribute to set Category Mapping between Magento and Ricardo, set also a button to popup the window to display the ricardo category view

---
+++ src/app/code/community/Diglin/Ricento/sql/diglin_ricento_resource_setup/upgrade-0.0.1-0.0.2.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Diglin GmbH - Switzerland
+ *
+ * @category    Diglin
+ * @package     Diglin_Ricento
+ * @copyright   Copyright (c) 2011-2014 Diglin (http://www.diglin.com)
+ */ 
+/* @var $installer Mage_Catalog_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$entityTypeId = $installer->getEntityTypeId(Mage_Catalog_Model_Category::ENTITY);
+$attributeSetId   = $installer->getDefaultAttributeSetId($entityTypeId);
+$attributeGroupId = $installer->getAttributeGroupId($entityTypeId, $attributeSetId, 'General Information');
+
+$installer->addAttribute(Mage_Catalog_Model_Category::ENTITY, 'ricardo_category', array(
+    'input_renderer'    => 'diglin_ricento/adminhtml_catalog_category_form_renderer_mapping',
+    'type'              => 'int',
+    'label'             => 'Ricardo Category',
+    'note'              => 'Map this current Magento category with one of Ricardo. It will facilitate you the creation of product listing.',
+    'input'             => 'text',
+    'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'required'          => false,
+    'user_defined'      => false,
+    'unique'            => false,
+    'default'           => ''
+));
+
+$installer->addAttributeToGroup(
+    $entityTypeId,
+    $attributeSetId,
+    $attributeGroupId,
+    'ricardo_category'
+);
+
+$installer->endSetup();
