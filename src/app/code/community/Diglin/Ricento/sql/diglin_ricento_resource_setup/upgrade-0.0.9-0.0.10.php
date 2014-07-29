366022660c29cab40172b61ff00f3af204c7d147
Sylvain Rayé <sly@diglin.com>
2014-07-29 20:39:08 +0200

    [RIC-37] fix wrong column type varchar to varbinary. Adding a new column in this way, you cannot use varchar type directly.

--- src/app/code/community/Diglin/Ricento/sql/diglin_ricento_resource_setup/upgrade-0.0.9-0.0.10.php
+++ src/app/code/community/Diglin/Ricento/sql/diglin_ricento_resource_setup/upgrade-0.0.9-0.0.10.php
@@ -23,15 +23,15 @@ $installer->getConnection()->addColumn($salesOptionsTable, 'product_warranty', a
     'comment' => 'Product Warranty'));
 
 $installer->getConnection()->addColumn($salesOptionsTable, 'product_condition', array(
-    'type' => Varien_Db_Ddl_Table::TYPE_VARCHAR,
-    'length' => '50',
+    'type' => Varien_Db_Ddl_Table::TYPE_VARBINARY,
+    'length' => 50,
     'nullable' => true,
     'after' => 'product_warranty',
     'comment' => 'Product Condition'));
 
 $installer->getConnection()->addColumn($salesOptionsTable, 'product_condition_source_attribute_code', array(
-    'type' => Varien_Db_Ddl_Table::TYPE_VARCHAR,
-    'length' => '255',
+    'type' => Varien_Db_Ddl_Table::TYPE_VARBINARY,
+    'length' => 255,
     'nullable' => true,
     'after' => 'product_condition',
     'comment' => 'Product Condition Attribute Code'));
