1f4704771217fd1d497e5789d2d3f91ef2682f4d
Sylvain Rayé <sly@diglin.com>
2014-07-29 19:53:09 +0200

    [RIC-37] Remove the category mapping button in the product grid view page. Mapping is moved to the Magento category pages

--- src/app/code/community/Diglin/Ricento/Block/Adminhtml/Products/Listing.php
+++ src/app/code/community/Diglin/Ricento/Block/Adminhtml/Products/Listing.php
@@ -20,20 +20,8 @@ class Diglin_Ricento_Block_Adminhtml_Products_Listing extends Mage_Adminhtml_Blo
         $this->_headerText = $this->__('Products Listing');
         $this->_addButtonLabel = $this->__('Create new listing');
 
-        $this->_addButton('category_mapping', array(
-                'label'     => $this->__('Category Mapping'),
-                'onclick'   => 'setLocation(\'' . $this->getCategoryMapUrl() .'\')',
-                'class'     => 'mapping',
-            )
-            ,0,1);
-
         parent::__construct();
         $this->_updateButton('add', 'onclick', 'Ricento.newListingPopup()');
-    }
-
-    public function getCategoryMapUrl()
-    {
-        return $this->getUrl('*/products_category/mapping');
     }
 
     public function getCreateUrl()
