d73a84e74de7e5010f60fd85c41c33b824abf488
Sylvain Rayé <sly@diglin.com>
2014-08-06 23:24:41 +0200

    [RIC-43] Add instructions in popup window added for the user

--- src/app/code/community/Diglin/Ricento/Block/Adminhtml/Products/Category/Tree.php
+++ src/app/code/community/Diglin/Ricento/Block/Adminhtml/Products/Category/Tree.php
@@ -21,5 +21,15 @@ class Diglin_Ricento_Block_Adminhtml_Products_Category_Tree extends Mage_Adminht
         $this->removeButton('reset');
         $this->removeButton('delete');
         $this->_headerText = $this->__('Add products from selected categories');
+
+        Mage::getSingleton('adminhtml/session')->addNotice($this->getInstructionsText());
+    }
+
+    public function getInstructionsText()
+    {
+        $instructions = $this->__('Only product types supported, enabled and not listed in other product listing will be added.');
+        $instructions .= '&nbsp;';
+        $instructions .= $this->__('Be aware, you have also to select the sub-categories, otherwise selecting only a top category won\'t allow to add products of the subcategories.');
+        return $instructions;
     }
 }
