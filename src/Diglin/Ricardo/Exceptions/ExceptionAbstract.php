fe99af4bbb37353af23c3e1e2dd32bcee4af7208
Sylvain Rayé <sly@diglin.com>
2014-09-14 22:10:24 +0200

    Change the exception abstract to get the correct enums error code when needed

--- src/Diglin/Ricardo/Exceptions/ExceptionAbstract.php
+++ src/Diglin/Ricardo/Exceptions/ExceptionAbstract.php
@@ -10,7 +10,37 @@
 
 namespace Diglin\Ricardo\Exceptions;
 
+/**
+ * Class ExceptionAbstract
+ * @package Diglin\Ricardo\Exceptions
+ */
 class ExceptionAbstract extends \Exception
 {
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
+    {
+        $message = $this->_getMessage($code, $message);
+        return parent::__construct($message, $code, $previous);
+    }
 
+    /**
+     * @param $code
+     * @param string $defaultMessage
+     * @return bool|string
+     */
+    protected function _getMessage($code, $defaultMessage = '')
+    {
+        $class = get_class($this);
+        $class = str_replace('Exceptions', 'Enums', $class);
+        $class = str_replace('Exception', 'Errors', $class);
+
+        if (class_exists($class)) {
+            /* @var $class \Diglin\Ricardo\Enums\AbstractEnums */
+            $message = $class::getLabel($code);
+            if (!empty($message)) {
+                return $defaultMessage . ' ' . $message;
+            }
+        }
+
+        return $defaultMessage;
+    }
 }
