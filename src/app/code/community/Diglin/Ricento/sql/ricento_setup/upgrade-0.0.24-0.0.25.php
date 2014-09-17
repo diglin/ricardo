f399e526f61edb215868b9835a6f2d779072b6fa
Sylvain Rayé <sly@diglin.com>
2014-09-17 09:09:57 +0200

    [RIC-55] Add is_planned column to product listing item

---
+++ src/app/code/community/Diglin/Ricento/sql/ricento_setup/upgrade-0.0.24-0.0.25.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Diglin GmbH - Switzerland
+ *
+ * @author Sylvain Rayé <sylvain.raye at diglin.com>
+ * @category    Ricento
+ * @package     Diglin_Ricardo
+ * @copyright   Copyright (c) 2011-2014 Diglin (http://www.diglin.com)
+ */
+/* @var $installer Mage_Catalog_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$itemTable = $installer->getTable('diglin_ricento/products_listing_item');
+
+$installer->getConnection()->addColumn($itemTable, 'is_planned', array(
+    'type' => Varien_Db_Ddl_Table::TYPE_SMALLINT,
+    'nullable' => true,
+    'unsigned' => true,
+    'after' => 'ricardo_article_id',
+    'comment' => 'Is Planned'));
+
+$installer->endSetup();
