58a2ccdcec8886f6204d30c66d088f1d77423552
Sylvain Rayé <sly@diglin.com>
2014-10-06 22:43:01 +0200

    Factorize Sync List Dispatcher and add if articles are unsold to allow to change article id and status

--- src/app/code/community/Diglin/Ricento/Model/Dispatcher/Sync/List.php
+++ src/app/code/community/Diglin/Ricento/Model/Dispatcher/Sync/List.php
@@ -12,6 +12,7 @@
 use \Diglin\Ricardo\Enums\Article\ArticlesTypes;
 use \Diglin\Ricardo\Managers\SellerAccount\Parameter\OpenArticlesParameter;
 use \Diglin\Ricardo\Managers\SellerAccount\Parameter\SoldArticlesParameter;
+use \Diglin\Ricardo\Managers\SellerAccount\Parameter\UnsoldArticlesParameter;
 
 /**
  * Class Diglin_Ricento_Model_Dispatcher_Sync_List
@@ -96,60 +97,57 @@ class Diglin_Ricento_Model_Dispatcher_Sync_List extends Diglin_Ricento_Model_Dis
     {
         $jobListing = $this->_currentJobListing;
 
-        /* @var $sellerAccount Diglin_Ricento_Model_Api_Services_SellerAccount */
-        $sellerAccount = Mage::getSingleton('diglin_ricento/api_services_selleraccount');
-
         $itemCollection = $this->_getItemCollection(array(Diglin_Ricento_Helper_Data::STATUS_LISTED), $jobListing->getLastItemId());
         $itemCollection->addFieldToFilter('is_planned', 1);
-
-        $helper = Mage::helper('diglin_ricento');
 
         /* @var $item Diglin_Ricento_Model_Products_Listing_Item */
         foreach ($itemCollection->getItems() as $item) {
 
-            $hasSuccess = false;
             $article = null;
+            $isUnsold =  false;
             $openParameter = new OpenArticlesParameter();
             $openParameter->setInternalReferenceFilter($item->getInternalReference());
 
             try {
-                $openArticles = $sellerAccount->getServiceModel()->getOpenArticles($openParameter);
+                $openArticles = $this->_getSellerAccount()->getServiceModel()->getOpenArticles($openParameter);
 
                 /**
                  * Get Article information from OpenArticles method
                  */
                 if (count($openArticles['OpenArticles']) > 0) {
                     $article = Mage::helper('diglin_ricento')->extractData($openArticles['OpenArticles'][0]); // Only one element expected but more may come
-                } else {
-                    /**
-                     * We may have missed the article Id value changes in OpenArticles method, due to sales for example
-                     * so we try to get it from sold articles method
-                     */
-                    $soldArticlesParameter = new SoldArticlesParameter();
+                }
 
-                    /**
-                     * Set date to filter e.g. last day. Do not use a higher value as the minimum sales duration is 1 day,
-                     * we prevent to have conflict with several sold articles having similar internal reference
-                     */
-                    $soldArticlesParameter
-                        ->setInternalReferenceFilter($item->getInternalReference())
-                        ->setMinimumEndDate($helper->getJsonDate(time() - (1 * 24 * 60 * 60)));
+                /**
+                 * We may have missed the article Id value changes in OpenArticles method, due to sales for example
+                 * so we try to get it from sold articles method
+                 */
+                if (is_null($article)) {
+                    $article = $this->_getSoldArticles($item);
+                }
 
-                    $articles = $sellerAccount->getServiceModel()->getSoldArticles($soldArticlesParameter);
-                    if (count($articles) > 0) {
-                        $article = $helper->extractData($articles[0]);
+                /**
+                 * It's maybe not sold
+                 */
+                if (is_null($article)) {
+                    $article = $this->_getUnsoldArticles($item);
+                    if (!is_null($article)) {
+                        $isUnsold = true;
                     }
                 }
 
                 if ($article) {
-
                     /**
                      * Get the new ricardo article id if the article was planned before
                      */
-                    if ($article->getArticleId()) {
+                    if ($article->getArticleId() && !$isUnsold) {
                         $this->_itemStatus = Diglin_Ricento_Model_Products_Listing_Log::STATUS_SUCCESS;
-                        $this->_itemMessage = array('success' => $this->_getHelper()->__('The product has been updated to its new article id'));
+                        $this->_itemMessage = array('success' => $this->_getHelper()->__('The product is now live on ricardo.ch'));
                         $item->getResource()->saveCurrentItem($item->getId(), array('is_planned' => 0, 'ricardo_Article_id' => $article->getArticleId()));
+                    } elseif ($article->getArticleId() && $isUnsold) {
+                        $this->_itemStatus = Diglin_Ricento_Model_Products_Listing_Log::STATUS_SUCCESS;
+                        $this->_itemMessage = array('success' => $this->_getHelper()->__('Sorry, the product has not been sold'));
+                        $item->getResource()->saveCurrentItem($item->getId(), array('status' => Diglin_Ricento_Helper_Data::STATUS_STOPPED, 'is_planned' => 0, 'ricardo_Article_id' => $article->getArticleId()));
                     }
                 }
             } catch (Exception $e) {
@@ -186,5 +184,59 @@ class Diglin_Ricento_Model_Dispatcher_Sync_List extends Diglin_Ricento_Model_Dis
         }
 
         return $this;
+    }
+
+    /**
+     * @param $item
+     * @return null|Varien_Object
+     */
+    protected function _getSoldArticles($item)
+    {
+        $article = null;
+        $soldArticlesParameter = new SoldArticlesParameter();
+
+        /**
+         * Set date to filter e.g. last day. Do not use a higher value as the minimum sales duration is 1 day,
+         * we prevent to have conflict with several sold articles having similar internal reference
+         */
+        $soldArticlesParameter
+            ->setInternalReferenceFilter($item->getInternalReference())
+            ->setMinimumEndDate($this->_getHelper()->getJsonDate(time() - (1 * 24 * 60 * 60)));
+
+        $articles = $this->_getSellerAccount()->getServiceModel()->getSoldArticles($soldArticlesParameter);
+        if (count($articles) > 0) {
+            $article = $this->_getHelper()->extractData($articles[0]);
+        }
+
+        return $article;
+    }
+
+    /**
+     * @param $item
+     * @return null|Varien_Object
+     */
+    protected function _getUnsoldArticles($item)
+    {
+        $article = null;
+        $unsoldArticlesParameter = new UnsoldArticlesParameter();
+
+        $unsoldArticlesParameter
+            ->setInternalReferenceFilter($item->getInternalReference())
+            ->setMinimumEndDate($this->_getHelper()->getJsonDate(time() - (1 * 24 * 60 * 60)));
+
+        $articles = $this->_getSellerAccount()->getServiceModel()->getUnsoldArticles($unsoldArticlesParameter);
+        if (count($articles) > 0) {
+            $article = $this->_getHelper()->extractData($articles[0]);
+        }
+
+        return $article;
+    }
+
+    /**
+     * @return Diglin_Ricento_Model_Api_Services_Selleraccount
+     */
+    protected function _getSellerAccount()
+    {
+        return Mage::getSingleton('diglin_ricento/api_services_selleraccount');
     }
 }
