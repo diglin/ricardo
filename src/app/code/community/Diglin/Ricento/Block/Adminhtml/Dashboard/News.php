7f65580319c6911eaff3d3a20a655426f93ddeda
Sylvain Rayé <sly@diglin.com>
2014-10-21 09:32:35 +0200

    Enhance and add the ricardo rss feed link

--- src/app/code/community/Diglin/Ricento/Block/Adminhtml/Dashboard/News.php
+++ src/app/code/community/Diglin/Ricento/Block/Adminhtml/Dashboard/News.php
@@ -14,10 +14,19 @@ class Diglin_Ricento_Block_Adminhtml_Dashboard_News extends Mage_Adminhtml_Block
         try {
             $count = Mage::getStoreConfig(self::XML_PATH_NEWS_COUNT);
             $feed = Zend_Feed_Reader::import(Mage::getStoreConfig(self::XML_PATH_NEWS_FEED));
+
+            /* @var $feedItem Zend_Feed_Reader_Entry_Rss */
             foreach ($feed as $feedItem) {
                 if ($count-- <= 0) {
                     break;
                 }
+
+                $feedUrl = parse_url(Mage::getStoreConfig(self::XML_PATH_NEWS_FEED));
+                $baseUrl = $feedUrl['scheme'] . '://' . $feedUrl['host'];
+                $description = str_replace('src="/', 'src="'. $baseUrl .'/', $feedItem->getDescription());
+
+                $feedItem = new Varien_Object(array('title' => $feedItem->getTitle(), 'description' => $description, 'link' => $feedItem->getLink()));
+
                 $newsEntries[] = $feedItem;
             }
         } catch (Exception $e) {
