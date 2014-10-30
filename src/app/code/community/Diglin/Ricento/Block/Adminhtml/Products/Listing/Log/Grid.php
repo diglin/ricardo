03c9c05d380d068d022ecb7a22d2b332c92b52ce
Sylvain Rayé <sly@diglin.com>
2014-10-30 11:11:46 +0100

    Fix missing display of notice messages

--- src/app/code/community/Diglin/Ricento/Block/Adminhtml/Products/Listing/Log/Grid.php
+++ src/app/code/community/Diglin/Ricento/Block/Adminhtml/Products/Listing/Log/Grid.php
@@ -190,6 +190,7 @@ class Diglin_Ricento_Block_Adminhtml_Products_Listing_Log_Grid extends Mage_Admi
             $messages = Mage::helper('core')->jsonDecode($value);
             isset($messages['errors']) && count($messages['errors']) > 0 && $output .= '<div id="message-errors"' . $row->getId() . ' class="message_errors">' . implode('<br>', (array) $messages['errors']) . '</div>';
             isset($messages['warnings']) && count($messages['warnings']) > 0 && $output .= '<div id="message-warnings"' . $row->getId() . ' class="message_warnings">' . implode('<br>', (array) $messages['warnings']) . '</div>';
+            isset($messages['notice']) && count($messages['notice']) > 0 && $output .= '<div id="message-notice"' . $row->getId() . ' class="message_notice">' . implode('<br>', (array) $messages['notice']) . '</div>';
             isset($messages['success']) && count($messages['success']) > 0 && $output .= '<div id="message-success"' . $row->getId() . ' class="message_success">' . implode('<br>', (array) $messages['success']) . '</div>';
         }
         return $output;
