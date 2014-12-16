4e1dcf141bcd46aebe5a238718f41f2b2dd12d7a
Sylvain Rayé <sylvain.raye@rissip.com>
2014-12-16 12:47:07 +0100

    update git ignore + add command to generate code sniffer

--- bin/generatedoc.sh
+++ bin/generatedoc.sh
@@ -4,3 +4,4 @@
 phploc.phar src --log-xml phploc.xml
 phpmd.phar src xml rulesets/codesize.xml >> pmd.xml
 phpdox
+phpcs --standard=PSR2 --extensions=php --report=csv --report-file=codesniffer_report.csv src/Diglin/
