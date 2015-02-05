17cc8fe2d7fb02ee28ad33b68b9966652ce28dff
Sylvain Rayé <sly@diglin.com>
2015-02-05 13:02:27 +0100

    Change license from OSL v3 to GPL v3

--- src/Diglin/Ricardo/Api.php
+++ src/Diglin/Ricardo/Api.php
@@ -2,11 +2,21 @@
 /**
  * Diglin GmbH - Switzerland
  *
- * @author Sylvain Rayé <support at diglin.com>
+ * This file is part of a Diglin GmbH module.
+ *
+ * This Diglin GmbH module is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * @author      Sylvain Rayé <support at diglin.com>
  * @category    Diglin
  * @package     Diglin_Ricardo
  * @copyright   Copyright (c) 2011-2015 Diglin (http://www.diglin.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license     http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
  */
 namespace Diglin\Ricardo;
 
