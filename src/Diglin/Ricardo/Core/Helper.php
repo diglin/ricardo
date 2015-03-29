a83cf461adba4eb3524f223d8481e5ab72456593
Sylvain Rayé <sly@diglin.com>
2015-03-29 17:46:24 +0200

    Force string to lower for Guid

--- src/Diglin/Ricardo/Core/Helper.php
+++ src/Diglin/Ricardo/Core/Helper.php
@@ -100,9 +100,9 @@ class Helper
     public static function guid()
     {
         if (function_exists('com_create_guid') === true) {
-            return trim(com_create_guid(), '{}');
+            return strtolower(trim(com_create_guid(), '{}'));
         }
 
-        return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+        return strtolower(sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535)));
     }
 }
